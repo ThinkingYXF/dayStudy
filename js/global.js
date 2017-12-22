b2c40cd996f3d7b4934a69aef96a43eaca232548
yangxuefeng <yangxuefeng@microgravity.cn>
2017-12-22 17:59:10 +0800

    checkbox choose

--- js/global.js
+++ js/global.js
@@ -9,3 +9,22 @@ $.mask = function(){
 $.hideMask = function(){
     $('.mask').remove();
 }
+$.selected = function(params,allBtn, cancelBtn){
+    var chineseSettings = {
+        selectedAll: '全部',
+        selectedNone: '取消'
+    };
+    var usSettings = {
+        selectedAll: 'ALL',
+        selectedNone: 'CANCEL'
+    }
+    if(params['language'])
+        if(params['language'] === 'zh-CN')
+            params['language'] = chineseSettings;
+        else
+            params['language'] = usSettings;
+    else
+        params['language'] = usSettings;
+    $(allBtn).text(params['language']['selectedAll']);
+    $(cancelBtn).text(params['language']['selectedNone']);
+}
