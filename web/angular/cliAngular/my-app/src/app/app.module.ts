0d935c42275bb1d8a082443d39259ad4ceb01ef4
yangxuefeng <yangxuefeng@microgravity.cn>
2018-03-15 16:52:08 +0800

    the latest angular study

---
+++ web/angular/cliAngular/my-app/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService,{ dataEncapsulation: false }
+    )
+  ],
+  providers: [ HeroService, MessageService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
