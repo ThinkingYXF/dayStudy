f6aac37a23a08809c1f83b37edb6aabd76754799
thinkingYXF <thinking.yxf@gmail.com>
2018-05-24 12:44:10 +0800

    test

--- python/demo.py
+++ python/demo.py
@@ -1,5 +1,5 @@
-str = 'Hello World'
-print(str)
+# str = 'Hello World'
+# print(str)
 def fib(n):
 	a,b = 0,1
 	while(b < n):
@@ -9,3 +9,11 @@ def fib(n):
 fib(100)
 from math import pi
 print(pi)
+
+for x in range(2,100):
+	for n in range(2, x):
+		if(x % n == 0):
+			break
+	else:
+		print(x, end=',');
+print([str(round(pi, i)) for i in range(1,5)])
