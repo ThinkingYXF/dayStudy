daaa162a131e00518a093a76a21c543001884a1a
thinkingYXF <thinking.yxf@gmail.com>
2018-05-28 18:46:02 +0800

    vue

---
+++ vue/my-project/src/components/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="wrapper">
+		<h1>{{ title }}</h1>
+		<p>{{ content }}</p>
+	</div>
+</template>
+<script>
+var obj = {
+	title: 'home',
+	content: '阿三等奖哈达和'
+}
+export default {
+	name: 'Home',
+	data(){
+		return obj;
+	}
+}
+</script>
+
+<style scoped>
+	.wrapper > h1 {
+		text-align: center;
+		color: aquamarine;
+	}
+</style>
+
--- vue/my-project/src/router/index.js
+++ vue/my-project/src/router/index.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/home/Home'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     }
